3dc32428291f2a6ac95f5b5d4771189ea2201412
Ellen Rushe <ellen.rushe@ucdconnect.ie>
2021-03-29 13:53:02 +0100

    updated results with pr-auc

--- run_scripts/ae_sound_film_embed_64.sh
+++ run_scripts/ae_sound_film_embed_64.sh
@@ -9,7 +9,7 @@ best_ckpt_64=$(cat 'logs/mlp_sound_64/MLP_sound_64.json' | python -c 'import jso
 # Runs from 1 to $NUM_ITERS inclusive
 for i in $(seq "$NUM_ITERS")
 do 
- 	python train.py AE_sound_FiLM_embed_64 "" $i
+ 	#python train.py AE_sound_FiLM_embed_64 "" $i
 	val_json="logs/ae_sound_film_embed_64/AE_sound_FiLM_embed_64__$i.json"
 	python  eval_embed.py  $val_json $i --window_size 50  --decay_factor 0.1 --context_ckpt_name "checkpoint_MLP_sound_64_epoch_$best_ckpt_64"
 done
