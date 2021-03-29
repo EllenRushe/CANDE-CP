0b555bad86b06180e6297e0c43e87a96ac88ca3b
ellenrushe@gmail.com <ellenrushe@gmail.com>
2021-03-29 15:02:21 +0100

    Uncommented training for running training-val-eval ragther than just eval on its own.

--- run_scripts/ae_sound_all.sh
+++ run_scripts/ae_sound_all.sh
@@ -7,7 +7,7 @@ fi
 # Runs from 1 to $NUM_ITERS inclusive
 for i in $(seq "$NUM_ITERS")
 do 
-	#python train.py AE_sound_all "" $i
+	python train.py AE_sound_all "" $i
 	val_json="logs/ae_sound_all/AE_sound_all__$i.json"
 	python eval.py $val_json $i
 done
--- run_scripts/aes_sound.sh
+++ run_scripts/aes_sound.sh
@@ -11,7 +11,7 @@ do
 	# Runs from 1 to $NUM_ITERS inclusive
 	for context in {0..15}
 	do 
-		#python train.py AE_sound $context $i
+		python train.py AE_sound $context $i
 		val_json="logs/ae_sound/AE_sound_"$context"_"$i".json"
 		python eval.py $val_json $i
 	done
